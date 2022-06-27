d1984fba8a9e622938b59b2ad6fa3eeb02670e75
FBOELKE <fboelke@emea.corpdir.net>
2022-06-27 09:48:45 +0200

    CleaningUp index.html and index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,19 +1,7 @@
 import { performAction } from './js/app.js';
-// import { handleSubmit } from './js/formHandler';
 
-// console.log(checkForName);
-
-alert('TRAVEL APP IS ON');
-console.log('CHANGE!!');
+// alert('TRAVEL APP IS ON');
+// console.log('CHANGE!!');
 
 import './styles/index.scss';
-// import './styles/style.scss';
-
-// import './styles/resets.scss';
-// import './styles/base.scss';
-// import './styles/footer.scss';
-// import './styles/form.scss';
-// import './styles/header.scss';
-
-// export { checkForName, handleSubmit };
 export { performAction };
